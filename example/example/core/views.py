2ea9f5990d1fa273338b5b557809358b8ae8c435
Adam Johnson <me@adamj.eu>
2022-01-05 10:27:22 +0000

    Extract example favicon into view (#38)

--- example/example/core/views.py
+++ example/example/core/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views.decorators.http import require_GET
 
 
+@require_GET
 def index_django(request: HttpRequest) -> HttpResponse:
     return render(
         request,
@@ -12,6 +14,7 @@ def index_django(request: HttpRequest) -> HttpResponse:
     )
 
 
+@require_GET
 def index_jinja(request: HttpRequest) -> HttpResponse:
     return render(
         request,
@@ -20,4 +23,16 @@ def index_jinja(request: HttpRequest) -> HttpResponse:
             "title": "My Awesome Site",
         },
         using="jinja2",
+    )
+
+
+@require_GET
+def favicon(request: HttpRequest) -> HttpResponse:
+    return HttpResponse(
+        (
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">'
+            + '<text y=".9em" font-size="90">🔁</text>'
+            + "</svg>"
+        ),
+        content_type="image/svg+xml",
     )
