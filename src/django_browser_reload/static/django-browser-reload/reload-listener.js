a42861c3b4290f3b6e9a81dffec7e258a706304d
Adam Johnson <me@adamj.eu>
2022-02-03 09:41:53 +0000

    Move JavaScript linting to StandardJS (#65)

--- src/django_browser_reload/static/django-browser-reload/reload-listener.js
+++ src/django_browser_reload/static/django-browser-reload/reload-listener.js
@@ -1,28 +1,28 @@
-"use strict";
+'use strict'
 
 {
-  const dataset = document.currentScript.dataset;
-  const workerScriptPath = dataset.workerScriptPath;
-  const eventsPath = dataset.eventsPath;
+  const dataset = document.currentScript.dataset
+  const workerScriptPath = dataset.workerScriptPath
+  const eventsPath = dataset.eventsPath
 
   if (!window.SharedWorker) {
-    console.debug("😭 django-browser-reload cannot work in this browser.");
+    console.debug('😭 django-browser-reload cannot work in this browser.')
   } else {
     const worker = new SharedWorker(workerScriptPath, {
-      name: "django-browser-reload",
-    });
+      name: 'django-browser-reload'
+    })
 
-    worker.port.addEventListener("message", (event) => {
-      if (event.data === "Reload") {
-        location.reload();
+    worker.port.addEventListener('message', (event) => {
+      if (event.data === 'Reload') {
+        location.reload()
       }
-    });
+    })
 
     worker.port.postMessage({
-      type: "initialize",
-      eventsPath: eventsPath,
-    });
+      type: 'initialize',
+      eventsPath
+    })
 
-    worker.port.start();
+    worker.port.start()
   }
 }
