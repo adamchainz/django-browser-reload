b07576a8b048830b207a67e2a0222da36d9d2f93
Adam Johnson <me@adamj.eu>
2023-05-18 08:57:45 +0100

    Remove old django-stubs comment

--- tests/test_views.py
+++ tests/test_views.py
@@ -124,7 +124,6 @@ class EventsTests(SimpleTestCase):
         assert not views.should_reload_event.is_set()
 
     def test_success_template_change_with_gzip(self):
-        # https://github.com/typeddjango/django-stubs/pull/1421
         middleware = GZipMiddleware(views.events)
         request = RequestFactory(HTTP_ACCEPT_ENCODING="gzip").get("/")
         response = middleware(request)
